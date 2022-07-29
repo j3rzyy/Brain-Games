27bf017409d1fd55a9cefba6dec374d13d22c9a5
Egor Nikulin <egornik62@gmail.com>
2022-07-29 11:32:43 +0300

    the work on the "brain-gcd" is completed

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
 import { welcome } from '../src/brain-games.js';
+import { getGCD } from '../src/brain-gcd.js';
 
 welcome();
+getGCD();
--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -1,2 +1,29 @@
 import redlineSync from 'readline-sync';
+import { repeatsToWin } from '../src/brain-even.js';
+import { getRandomNum } from '../src/brain-even.js';
+import { maxValue } from '../src/brain-even.js';
+import { minValue } from '../src/brain-even.js';
 
+const getTrueAnswer = (firstVal, secondVal) => { // функция находит НОД двух чисел
+  if (secondVal > firstVal) return getTrueAnswer(secondVal, firstVal);
+  if (!secondVal) return firstVal;
+  return getTrueAnswer(secondVal, firstVal % secondVal);
+};
+
+export const getGCD = () => {
+  for (let i = 0; i < repeatsToWin;) {
+    console.log('Find the greatest common divisor of given numbers.');
+    const firstValue = getRandomNum(minValue, maxValue); // получение первого числа через функцию
+    const secondValue = getRandomNum(minValue, maxValue); // получение второго числа для НОД
+    console.log(`Question: ${firstValue} ${secondValue}`); // вывод в терминал чисел для нахождения НОД
+    const trueAnswer = getTrueAnswer(firstValue, secondValue);
+    const userAnswer = redlineSync.question('Your answer: ');
+    if (Number(userAnswer) === trueAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log('Incorrect!');
+      i = 0;
+    }
+  }
+};
