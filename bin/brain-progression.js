1f84a2e7807fcf0631c566a3abdee0f4f4eee75e
Egor Nikulin <egornik62@gmail.com>
2022-07-29 20:35:51 +0300

    the work on the "brain-progression" is completed

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { welcome } from "../src/brain-games.js";
+import { brainProgress } from "../src/brain-progression.js";
+
+welcome();
+brainProgress();
---
+++ src/brain-progression.js
@@ -0,0 +1,36 @@
+import redlineSync from 'readline-sync';
+import { repeatsToWin } from '../src/brain-even.js';
+import { getRandomNum } from '../src/brain-even.js';
+import { maxValue } from '../src/brain-even.js';
+import { minValue } from '../src/brain-even.js';
+
+const getProgress = () => {
+  const progress = [];
+  progress[0] = getRandomNum(minValue, maxValue); // первый случайный член прогрессии
+  const progressStep = getRandomNum(1, 10); // случайный шаг прогресии
+  const lengthOfProgress = 8; // длина прогресии задается строго
+  for (let i = 1; i < lengthOfProgress; i += 1) { // генерация прогрессии с учетом данных выше
+    progress[i] = progress[i - 1] + progressStep;
+  }
+  return progress;
+};
+
+export const brainProgress = () => {
+  let i = 0;
+  while (i < repeatsToWin) {
+    console.log('What number is missing in this progression?');
+    const progress = getProgress();
+    const randomMember = getRandomNum(0, progress.length); // выбор случайного члена прогрессии
+    const trueAnswer = progress[randomMember];
+    progress[randomMember] = '..';
+    console.log(`Question: ${progress.toString().split(',').join(' ')}`);
+    const userAnswer = redlineSync.question('Your answer: ');
+    if (Number(userAnswer) === trueAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log('Incorrect!');
+      i = 0;
+    }
+  }
+};
